61dcd2369f9b31ec0bcfb8cb2723fdead3b5afbc
niera26 <niera26@outlook.com>
2023-11-23 11:30:30 +0100

    better distribute test

--- test/Distribute.t.sol
+++ test/Distribute.t.sol
@@ -9,30 +9,38 @@ contract FeeTest is ERC20RewardsTest {
         address user1 = vm.addr(1);
         address user2 = vm.addr(2);
         address user3 = vm.addr(3);
+        address user4 = vm.addr(3);
 
         // set the buy fee to something easy to compute.
         token.setBuyFee(800, 200);
 
         // add some tax.
-        buyToken(user3, 1 ether);
+        buyToken(user4, 1 ether);
 
         // send the same value to two users.
-        uint256 balance = token.balanceOf(user3);
+        uint256 balance = token.balanceOf(user4);
+        uint256 quarter = balance / 4;
 
-        vm.prank(user3);
+        vm.prank(user4);
 
-        token.transfer(user1, balance / 2);
+        token.transfer(user1, quarter * 2);
 
-        vm.prank(user3);
+        vm.prank(user4);
 
-        token.transfer(user2, balance / 2);
+        token.transfer(user2, quarter);
+
+        vm.prank(user4);
+
+        token.transfer(user3, quarter);
 
         // two users should get the same rewards.
         token.distribute();
 
         assertGt(token.pendingRewards(user1), 0);
         assertGt(token.pendingRewards(user2), 0);
-        assertEq(token.pendingRewards(user1), token.pendingRewards(user2));
+        assertGt(token.pendingRewards(user3), 0);
+        assertEq(token.pendingRewards(user1), token.pendingRewards(user2) + token.pendingRewards(user3));
+        assertEq(token.pendingRewards(user2), token.pendingRewards(user3));
         assertGt(rewardToken.balanceOf(token.marketingWallet()), 0);
 
         // two users should claim the same amount.
@@ -44,11 +52,19 @@ contract FeeTest is ERC20RewardsTest {
 
         token.claim();
 
-        assertEq(rewardToken.balanceOf(user1), rewardToken.balanceOf(user2));
+        vm.prank(user3);
+
+        token.claim();
+
+        assertGt(rewardToken.balanceOf(user1), 0);
+        assertGt(rewardToken.balanceOf(user2), 0);
+        assertGt(rewardToken.balanceOf(user3), 0);
+        assertEq(rewardToken.balanceOf(user1), rewardToken.balanceOf(user2) + rewardToken.balanceOf(user3));
+        assertEq(rewardToken.balanceOf(user2), rewardToken.balanceOf(user3));
 
         // check marketing amount.
-        uint256 distributed =
-            rewardToken.balanceOf(user1) + rewardToken.balanceOf(user2) + rewardToken.balanceOf(token.marketingWallet());
+        uint256 distributed = rewardToken.balanceOf(user1) + rewardToken.balanceOf(user2) + rewardToken.balanceOf(user2)
+            + rewardToken.balanceOf(token.marketingWallet());
 
         assertApproxEqRel(rewardToken.balanceOf(token.marketingWallet()), distributed / 5, 0.01e18);
     }
