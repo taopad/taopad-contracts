eb9f27887df7e3b2f7391430bbe76aabb5890bd2
niera26 <niera26@outlook.com>
2023-12-14 19:23:41 +0100

    add comments

--- src/Taopad.sol
+++ src/Taopad.sol
@@ -3,6 +3,18 @@ pragma solidity ^0.8.23;
 
 import {ERC20Rewards} from "./ERC20Rewards.sol";
 
+/**
+ * Useful Links:
+ * 
+ * Website: https://taopad.io/
+ * X: https://x.com/TaoPadX
+ * 
+ * Expanding The Tao Ecosystem.
+ * $TAOPAD holders receive $wTAO
+ * 
+ *
+ */
+
 /// @title Taopad
 /// @author @niera26
 /// @notice buy and sell tax on this token with rewardToken as wTao
